23256106483c42437bb3625c52728bf35dacba0a
Corey <corey@undimk3>
2012-11-14 08:07:05 -0500

    More work on forth words

--- src/com/undi/javaforth/ForthExecutable.java
+++ src/com/undi/javaforth/ForthExecutable.java
@@ -2,5 +2,9 @@ package com.undi.javaforth;
 import java.util.Stack;
 
 public interface ForthExecutable{
+		/**
+			 Executes, returns an index increment
+			 ex: NEXT is 1, BRANCH is to where it needs to go, and EXIT is 0 (errors are through exceptions)
+		 **/
 		public int execute(Stack datStack, Stack retStack);
 }
